4e05aaf90f82b7bda51e203ff4be71402d87bad6
David Powers <david@1xrun.com>
2016-07-30 17:30:18 -0500

    new GuardianResponse test

--- tests/acceptance/GuardianResponseCept.php
+++ tests/acceptance/GuardianResponseCept.php
@@ -2,14 +2,12 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Test Guardian Response');
 
-$I->amOnPage('/guardian-response/?pgnumb=1&pnumb=1&pet_id=&pet_name=Nimh&pet_type=Rodent&user_email=dpowers@rezon8.net&prefix=%7Buser:prefix%7D&first_name=dpowers@rezon8.net&last_name=Powers&pguardian_email=dpowers@f4digital.com&pguardian_mobile_phone=(773)%20641-1561&pguardian_response=Accepted&pguardian_prefix=Mr.&pguardian_first_name=Donald&pguardian_last_name=Duck&eid=ozILpxqYrl3J2UoLK533%2F%2FGfV6SOOegHJD%2F%2BcjVBZXw%3D');
+$I->amOnPage('/guardian-response/?pgnumb=1&pnumb=1&pet_id=1896893806&pet_name=Araxara&pet_type=Bird&user_email=dpowers@rezon8.net&prefix=Mr.&first_name=Alpha&last_name=Beta&pguardian_email=dpowers@f4digital.com&pguardian_mobile_phone=(773)%20641-1561&pguardian_response=d&pguardian_prefix=Mr.&pguardian_first_name=Alef&pguardian_last_name=Anderson&eid=fwtBo5xPpCmHviQ4CHXsaLY56jmwFHYhM8%2B0aXz88dg%3D');
 $I->see('Pet Guardian Nomination');
-$I->fillField('input_15', '1');
 $I->click('Submit Response');
-$I->see('Congratulations on becoming a PetGuardian!');
+$I->see('We are sorry to hear that you cannot be a guardian ');
 
-$I->amOnPage('/guardian-response/?pgnumb=2&pnumb=1&pet_id=&pet_name=Nimh&pet_type=Rodent&user_email=dpowers@rezon8.net&prefix=&first_name=&last_name=&pguardian_email=dpowers@found4digital.com&pguardian_response=Declined&pguardian_prefix=Mr.&pguardian_first_name=Dizzy&pguardian_last_name=Dukes&pguardian_mobile_phone=(773)%20609-2730&eid=ozILpxqYrl3J2UoLK533%2F%2FGfV6SOOegHJD%2F%2BcjVBZXw%3D');
-$I->fillField('input_15', '0');
+$I->amOnPage('/guardian-response/?pgnumb=2&pnumb=1&pet_id=1896893806&pet_name=Araxara&pet_type=Bird&user_email=dpowers@rezon8.net&prefix=Mr.&first_name=Alpha&last_name=Beta&pguardian_email=dpowers@found4digital.com&pguardian_response=1&pguardian_prefix=Mrs.&pguardian_first_name=Bet&pguardian_last_name=Bagelson&pguardian_mobile_phone=(773)%20609-2730&eid=fwtBo5xPpCmHviQ4CHXsaLY56jmwFHYhM8%2B0aXz88dg%3D');
 $I->click('Submit Response');
 $I->see('Congratulations on becoming a PetGuardian!');
 
